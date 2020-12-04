bd61cdb94038b495e3bbd5d6d253aec122da780b
Griesenbeck <tabea.griesenbeck@bcw-gruppe.de>
2020-12-04 16:22:34 +0100

    Layout-Ergänzungen

---
+++ RENDER-THE-BOOK.R
@@ -0,0 +1,5 @@
+## Render to HTML
+rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
+
+## Render to pdf
+rmarkdown::render_site(output_format = 'bookdown::pdf_book', encoding = 'UTF-8')
